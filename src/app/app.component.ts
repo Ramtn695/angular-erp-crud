7def7610acd31121494214080e61cc01a77db0ee
Ramtn695 <ramaswamytnr@gmail.com>
2025-01-20 12:24:38 +0530

    Changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,NavBarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-app';
+}
